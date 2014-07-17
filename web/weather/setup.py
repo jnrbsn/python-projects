793950f17d276a2cd2cbd455641fdff352075f13
Jonathan Robson <jnrbsn@gmail.com>
2014-07-17 15:29:13 +0200

    Changed entry point name.

--- web/weather/setup.py
+++ web/weather/setup.py
@@ -21,7 +21,7 @@ setup(
     include_package_data=True,
     entry_points={
         'console_scripts': [
-            'weather=weather.app:run'
+            'weather-server=weather.app:run'
         ],
     },
 )
